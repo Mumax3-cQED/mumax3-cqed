c566c4a98355e35938347a779682024677ccfabe
sergiomtzlosa <sergiomtz.losa@gmail.com>
2021-11-26 18:54:55 +0100

    more content

--- make.bash
+++ make.bash
@@ -2,8 +2,8 @@
 
 CGO_CFLAGS_ALLOW='(-fno-schedule-insns|-malign-double|-ffast-math)'
 
-ln -sf $(pwd)/pre-commit .git/hooks/pre-commit || echo ""
-ln -sf $(pwd)/post-commit .git/hooks/post-commit || echo ""
+#ln -sf $(pwd)/pre-commit .git/hooks/pre-commit || echo ""
+#ln -sf $(pwd)/post-commit .git/hooks/post-commit || echo ""
 
 (cd cuda && ./make.bash)  || exit 1
 go install -v github.com/mumax/3/cmd/... || exit 1
