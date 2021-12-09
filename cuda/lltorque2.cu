064ac2e3c9b8061222f5e94a7c6add5e38b8939a
sergiomtzlosa <sergiomtz.losa@gmail.com>
2021-12-09 20:19:57 +0100

    compute new term in formula

--- cuda/lltorque2.cu
+++ cuda/lltorque2.cu
@@ -21,7 +21,8 @@ extern "C" __global__ void
 lltorque2(float* __restrict__  tx, float* __restrict__  ty, float* __restrict__  tz,
           float* __restrict__  mx, float* __restrict__  my, float* __restrict__  mz,
           float* __restrict__  hx, float* __restrict__  hy, float* __restrict__  hz,
-          float* __restrict__  alpha_, float alpha_mul, int N, float* __restrict__ dt, float* __restrict__ fixed_dt, float time, float wc, float brms_x, float brms_y, float brms_z, float* __restrict__ deltas) {
+          float* __restrict__  alpha_, float alpha_mul, int N,
+          float* __restrict__ dt, float* __restrict__ fixed_dt, float time, float wc, float brms_x, float brms_y, float brms_z, float* __restrict__ deltas) {
 
     int i =  ( blockIdx.y*gridDim.x + blockIdx.x ) * blockDim.x + threadIdx.x;
 
