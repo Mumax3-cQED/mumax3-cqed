55043d14da5f66666df13f5a3e75fb956e84ce7a
sergiomtzlosa <sergiomtz.losa@gmail.com>
2021-12-17 14:53:03 +0100

    compute new term in formula

--- cuda/mdatatemp.cu
+++ cuda/mdatatemp.cu
@@ -1,6 +1,3 @@
-
-
-
 extern "C" __global__ void
 mdatatemp(float* __restrict__  dst_x, float* __restrict__  dst_y, float* __restrict__  dst_z, float* __restrict__ sin_full_time, float* __restrict__ full_tau,
       float* __restrict__  mx_temp, float* __restrict__  my_temp, float* __restrict__  mz_temp, float wc, float dt, int N) {
