fa2bfe2d41598627cea87eba08daf8c8c0dc3546
sergiomtzlosa <sergiomtz.losa@gmail.com>
2022-04-18 09:26:43 +0200

    start test on discretized formula

--- cuda/lltorque2time.cu
+++ cuda/lltorque2time.cu
@@ -8,8 +8,6 @@
 #include <math.h>
 #include "constants.h"
 
-#define GS 2.0
-
 static __inline__ __device__ float3 operator*(const float3 &a, const float3 &b) {
   return make_float3(a.x * b.x, a.y * b.y, a.z * b.z);
 }
