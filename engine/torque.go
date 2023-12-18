fbc55c770d4e5feaff74e335fa8bc673b6f19483
sergiomtzlosa <sergiomtz.losa@gmail.com>
2023-12-18 09:30:50 +0100

    minor code review

--- engine/torque.go
+++ engine/torque.go
@@ -37,7 +37,7 @@ var (
 	Wc                           = NewScalarParam("Wc", "rad/s", "Resonant frequency of the cavity")
 	Kappa                        = NewScalarParam("Kappa", "rad/s", "Cavity dissipation")
 	NSpins          float64      = 0          // Number of spins
-	StartCheckpoint time.Time    = time.Now() // Starting date for mumax3 script to measure elapsed execution time, to set starting date anywhere in the  --> Start = now()
+	StartCheckpoint time.Time    = time.Now() // Starting date for mumax3 script to measure elapsed execution time, to set starting date anywhere in the  --> StartCheckpoint = now()
 	mem_term        *MEMORY_TERM = nil
 )
 
