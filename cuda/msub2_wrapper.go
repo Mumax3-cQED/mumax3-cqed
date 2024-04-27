53b125188c5e8b89d1e79f3680167533c30727ff
sergiomtzlosa <sergiomtz.losa@gmail.com>
2024-04-27 13:38:56 +0200

    assertion review for spins

--- cuda/msub2_wrapper.go
+++ cuda/msub2_wrapper.go
@@ -73,8 +73,6 @@ func k_msub2_async ( dst unsafe.Pointer, src1 unsafe.Pointer, fac1 float32, src2
 
 // maps compute capability on PTX code for msub2 kernel.
 var msub2_map = map[int]string{ 0: "" ,
-35: msub2_ptx_35 ,
-37: msub2_ptx_37 ,
 50: msub2_ptx_50 ,
 52: msub2_ptx_52 ,
 53: msub2_ptx_53 ,
@@ -85,123 +83,7 @@ var msub2_map = map[int]string{ 0: "" ,
 
 // msub2 PTX code for various compute capabilities.
 const(
-  msub2_ptx_35 = `
-.version 6.4
-.target sm_35
-.address_size 64
-
-	// .globl	msub2
-
-.visible .entry msub2(
-	.param .u64 msub2_param_0,
-	.param .u64 msub2_param_1,
-	.param .f32 msub2_param_2,
-	.param .u64 msub2_param_3,
-	.param .f32 msub2_param_4,
-	.param .u32 msub2_param_5
-)
-{
-	.reg .pred 	%p<2>;
-	.reg .f32 	%f<8>;
-	.reg .b32 	%r<9>;
-	.reg .b64 	%rd<11>;
-
-
-	ld.param.u64 	%rd1, [msub2_param_0];
-	ld.param.u64 	%rd2, [msub2_param_1];
-	ld.param.f32 	%f1, [msub2_param_2];
-	ld.param.u64 	%rd3, [msub2_param_3];
-	ld.param.f32 	%f2, [msub2_param_4];
-	ld.param.u32 	%r2, [msub2_param_5];
-	mov.u32 	%r3, %ctaid.y;
-	mov.u32 	%r4, %nctaid.x;
-	mov.u32 	%r5, %ctaid.x;
-	mad.lo.s32 	%r6, %r4, %r3, %r5;
-	mov.u32 	%r7, %ntid.x;
-	mov.u32 	%r8, %tid.x;
-	mad.lo.s32 	%r1, %r6, %r7, %r8;
-	setp.ge.s32	%p1, %r1, %r2;
-	@%p1 bra 	BB0_2;
-
-	cvta.to.global.u64 	%rd4, %rd2;
-	mul.wide.s32 	%rd5, %r1, 4;
-	add.s64 	%rd6, %rd4, %rd5;
-	ld.global.nc.f32 	%f3, [%rd6];
-	mul.f32 	%f4, %f3, %f1;
-	cvta.to.global.u64 	%rd7, %rd3;
-	add.s64 	%rd8, %rd7, %rd5;
-	ld.global.nc.f32 	%f5, [%rd8];
-	mul.f32 	%f6, %f5, %f2;
-	sub.f32 	%f7, %f4, %f6;
-	cvta.to.global.u64 	%rd9, %rd1;
-	add.s64 	%rd10, %rd9, %rd5;
-	st.global.f32 	[%rd10], %f7;
-
-BB0_2:
-	ret;
-}
-
-
-`
-   msub2_ptx_37 = `
-.version 6.4
-.target sm_37
-.address_size 64
-
-	// .globl	msub2
-
-.visible .entry msub2(
-	.param .u64 msub2_param_0,
-	.param .u64 msub2_param_1,
-	.param .f32 msub2_param_2,
-	.param .u64 msub2_param_3,
-	.param .f32 msub2_param_4,
-	.param .u32 msub2_param_5
-)
-{
-	.reg .pred 	%p<2>;
-	.reg .f32 	%f<8>;
-	.reg .b32 	%r<9>;
-	.reg .b64 	%rd<11>;
-
-
-	ld.param.u64 	%rd1, [msub2_param_0];
-	ld.param.u64 	%rd2, [msub2_param_1];
-	ld.param.f32 	%f1, [msub2_param_2];
-	ld.param.u64 	%rd3, [msub2_param_3];
-	ld.param.f32 	%f2, [msub2_param_4];
-	ld.param.u32 	%r2, [msub2_param_5];
-	mov.u32 	%r3, %ctaid.y;
-	mov.u32 	%r4, %nctaid.x;
-	mov.u32 	%r5, %ctaid.x;
-	mad.lo.s32 	%r6, %r4, %r3, %r5;
-	mov.u32 	%r7, %ntid.x;
-	mov.u32 	%r8, %tid.x;
-	mad.lo.s32 	%r1, %r6, %r7, %r8;
-	setp.ge.s32	%p1, %r1, %r2;
-	@%p1 bra 	BB0_2;
-
-	cvta.to.global.u64 	%rd4, %rd2;
-	mul.wide.s32 	%rd5, %r1, 4;
-	add.s64 	%rd6, %rd4, %rd5;
-	ld.global.nc.f32 	%f3, [%rd6];
-	mul.f32 	%f4, %f3, %f1;
-	cvta.to.global.u64 	%rd7, %rd3;
-	add.s64 	%rd8, %rd7, %rd5;
-	ld.global.nc.f32 	%f5, [%rd8];
-	mul.f32 	%f6, %f5, %f2;
-	sub.f32 	%f7, %f4, %f6;
-	cvta.to.global.u64 	%rd9, %rd1;
-	add.s64 	%rd10, %rd9, %rd5;
-	st.global.f32 	[%rd10], %f7;
-
-BB0_2:
-	ret;
-}
-
-
-`
-   msub2_ptx_50 = `
+  msub2_ptx_50 = `
 .version 6.4
 .target sm_50
 .address_size 64
