aed8c745312f1d0cb347845e961765b560ff6fb0
sergiomtzlosa <sergiomtz.losa@gmail.com>
2024-08-31 20:11:25 +0200

    big change unification kernel

--- cuda/cavity.go
+++ cuda/cavity.go
@@ -43,21 +43,3 @@ func AddCavity(dst, m, brms *data.Slice, wc, kappa MSlice, x0, p0, msatCell, del
 		Madd2(dst, dst, brms, 1.0, float32(G))
 	}
 }
-
-// func AddCavity2(dst, m, brms *data.Slice, wc, kappa MSlice, x0, p0, msatCell, deltah, ctime float64, mem *[2]float64, mesh *data.Mesh) {
-//
-// 	N := mesh.Size()
-//
-// 	kappa_temp := float64(kappa.Mul(0))
-// 	wc_temp := float64(wc.Mul(0))
-//
-// 	brms = data.Resample(brms, N) // reshape of OVF Brms file to mesh size
-// 	brms_m := float64(Dot(brms, m))
-//
-// 	(*mem)[0] += math.Exp(kappa_temp*ctime) * math.Sin(wc_temp*ctime) * brms_m * deltah
-// 	(*mem)[1] += math.Exp(kappa_temp*ctime) * math.Cos(wc_temp*ctime) * brms_m * deltah
-//
-// 	G := math.Exp(-kappa_temp*ctime) * (math.Cos(wc_temp*ctime)*(x0-msatCell*(*mem)[0]) - math.Sin(wc_temp*ctime)*(p0-msatCell*(*mem)[1]))
-//
-// 	Madd2(dst, dst, brms, 1.0, float32(G))
-// }
